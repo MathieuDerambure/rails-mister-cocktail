bca6bf22b23b51f5627062b76c5a603c473ba0e7
Mathieu Derambure <deramburemathieu@hotmail.com>
2018-08-16 12:01:58 +0200

    models ok

---
+++ app/models/cocktail.rb
@@ -0,0 +1,8 @@
+class Cocktail < ApplicationRecord
+  has_many :doses, dependent: :delete_all
+  has_many :ingredients, :through => :doses
+
+
+  validates :name, uniqueness: true
+  validates :name, presence: true
+end
